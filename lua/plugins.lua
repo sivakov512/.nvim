ac75b9957881633d175c5be398ecc0c480db3e42
Nikita Sivakov <sivakov512@icloud.com>
2022-04-08 00:24:01 +0300

    Initial config

---
+++ lua/plugins.lua
@@ -0,0 +1,5 @@
+return require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+
+	use 'doums/darcula'
+end)
---
+++ lua/settings.lua
@@ -0,0 +1,8 @@
+local opt = vim.opt
+
+vim.cmd('colorscheme darcula')
+
+opt.number = true
+opt.relativenumber = true
+
+
