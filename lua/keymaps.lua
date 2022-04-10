21242f7a4b2751f253c93897fef5e6307354183d
Nikita Sivakov <sivakov512@icloud.com>
2022-04-10 20:39:37 +0300

    A bit of Emacs-like navigtion in insert mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,8 @@ local default_opts = { noremap = true, silent = true }
 map('n', '<C-b>', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], default_opts)
 map('n', '<C-f>', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], default_opts)
 map('n', '<C-s>', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], default_opts)
+
+map('i', '<C-f>', '<Right>', default_opts)
+map('i', '<C-b>', '<Left>', default_opts)
+map('i', '<C-p>', '<Up>', default_opts)
+map('i', '<C-n>', '<Down>', default_opts)
